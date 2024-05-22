c3156fb02d7030480cd55b8f5977eeffd8717c82
Felipe <fefelbf@gmail.com>
2024-05-22 11:07:01 -0300

    comit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const restart = document.querySelector('.restart')
 const gameOver = document.querySelector('.game-over');   
 
 audioStart = new Audio('images/goons-electronic-march-video-game-rpg-trolls-ogres-orcs-attack-145267.mp3')
+audioFala = new Audio('images/mario-fala.mp3')
 audioGameOver = new Audio('images/D2ZZHGM-game-over.mp3')
 audioCoin = new Audio('images/retro-video-game-coin-pickup-38299.mp3')
 
@@ -23,6 +24,8 @@ const startGame = () =>{
     choseDifficult.style.display = 'none';
 
     audioStart.play() //audio
+    audioFala.play()
+    audioFala.loop(0);
 }
 
 //restart
@@ -49,8 +52,8 @@ const loop = setInterval(() =>{
     const points = document.querySelector('.pontuação');
     points.innerHTML = `${marioPositionTwo}`
 
-    const coin = document.querySelector('.coin');
-    
+    const coin = document.querySelector('.div-coin');
+
     if(pipePosition <= 150 && pipePosition > 0 && marioPosition < 80){
         pipe.classList.remove('pipe-animation');
         pipe.style.left = `${pipePosition}px`;
@@ -67,13 +70,14 @@ const loop = setInterval(() =>{
         restart.style.display = 'block';
     }
     if(marioPosition > 80){
-        coin.style.display = 'block';
-        audioCoin.play()
+        coin.style.display = 'flex';
         coinContagem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="gold" class="bi bi-coin" viewBox="0 0 16 16">
             <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518z"/>
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11m0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12"/>
-            </svg> +1`;
+            </svg> +${coinValue}`;
+
+        audioCoin.play()
     } else{
         coin.style.display = 'none';
         coinContagem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" class="bi bi-coin" viewBox="0 0 16 16">
@@ -136,3 +140,10 @@ closeChosenHard.addEventListener('click', function(){
 
     audioStart.play() //audio
 })
+
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+let coinValue = 1;
+
+if(marioPosition >80){
+    coinValue = +1;
+}
